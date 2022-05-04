f38e885ed3d78737d6ba5cd022a0def7c0710af2
HoangKolya <kolyaramen@gmail.com>
2022-05-04 21:14:42 +0300

    first commit

---
+++ bin/Debug/net5.0/gits.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitStudy
+{
+    class gits
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ script.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStudy
+{
+    class script
+    {
+        int a = 2;
+        int b = 102;
+        
+    }
+}
